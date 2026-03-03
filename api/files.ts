8e44743782777c39080e2e678bf8d791900301b7
Marlon Torio <mvltorio@dswd.gov.ph>
2026-03-03 11:02:48 +0800

    Fix files API to return array

--- api/files.ts
+++ api/files.ts
@@ -1,3 +1,8 @@
-export default function handler(req: any, res: any) {
-  res.status(200).json({ message: "OK" });
+export default async function handler(req: any, res: any) {
+  if (req.method === 'GET') {
+    // Return empty array instead of object
+    return res.status(200).json([]);
+  }
+
+  return res.status(405).json({ message: 'Method not allowed' });
 }
